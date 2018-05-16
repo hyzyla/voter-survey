0ab9ae4767dd07f4f7142c450ffb888c6c4f1aac
Yevhenii <hyzyla@gmail.com>
2018-05-17 01:17:37 +0300

    f

--- account/serializers.py
+++ account/serializers.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User, Group
 
 
 class UserSerializer(serializers.ModelSerializer):
+    groups = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field='name'
+     )
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'groups', 'is_staff', 'is_superuser')
